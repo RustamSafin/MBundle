b5457a0da7a59e551f1974017bf84e4545355c8f
RustamSafin <last.safin@yandex.ru>
2018-01-14 01:33:44 +0300

    add fix

--- Service/EntityService.php
+++ Service/EntityService.php
@@ -61,6 +61,7 @@ class EntityService
             return new Response('no such entity');
         }
         $repository = $this->entityManager->getRepository($classMeta->getName());
+        dump($repository->find(''.$id));
         return $repository->find(''.$id);
     }
     public function save($entityName, $content ) {
